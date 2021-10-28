31e2f6b646794ca849b1d3d6faf8f30203d34842
lmartin23 <lmaartin997@gmail.com>
2021-10-28 15:51:29 -0300

    Version Base - Estable

---
+++ LaboratorioNoSQL/Models/DtoUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratorioNoSQL.Models
+{
+    public class DtoUsuario
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public ICollection<string> Roles { get; set; }
+    }
+}
